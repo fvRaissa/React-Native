8e8f9838e8178f1e915fa52ec949b4fb8e3b49c2
fvRaissa <93327739+fvRaissa@users.noreply.github.com>
2023-02-17 12:34:54 -0300

    Create estilo.js

---
+++ flatlist/estilos/estilo.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+       container:{
+           flex:1,
+           backgroundColor: '#fff',
+           //aligntems: 'center,
+        justifyContent:  'center',
+       },
+       teste:{
+           backgroundColor: '#dcdcdc',
+           marginVertical:5,
+           marginHorizontal:7,
+           paddinHorizontal:10,
+           paddinHorizontal:10,
+           borderWidth:1,
+           borderColor: '#FF0000',
+       },
+       
+   })
