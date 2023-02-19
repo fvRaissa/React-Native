93ab6bf4dd77aa152ee5a1d13ec052e9e0cff28d
fvRaissa <93327739+fvRaissa@users.noreply.github.com>
2023-02-18 23:20:22 -0300

    Add files via upload

---
+++ calculadora/styles/Style.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'#F0F1F5'
+    },
+
+    div1:{
+        flex:1,
+        backgroundColor:'transparent',
+        
+    },
+    resultado:{
+        
+    },
+    div2:{
+        flex:2,
+        backgroundColor:'#ffffff',
+        height:'65%',
+        flexDirection:'column'
+    },
+    linha1:{
+        height:"14%",
+        flexDirection:'row',
+        marginTop:"7%",
+        marginLeft:"1%"
+    },
+    linha:{
+        height:"17.5%",
+        flexDirection:'row',
+        marginTop:"2%",
+        marginLeft:"1%"
+    },
+    botao:{
+        marginLeft:"2%",
+        marginRight:"1%",
+        borderColor:'#000000',
+        borderWidth:'2px',
+        borderRadius:15,
+        width:"21.5%",
+        height:"90%",
+        alignItems:'center',
+    
+    },
+})
